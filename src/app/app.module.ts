efa73c04d7e2f3250842c8142f9767f14ce7f38c
Olya <olyabusol1605@gmail.com>
2021-01-31 23:38:36 +0200

    AngularLesson3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './component/home/home.component';
+import {RouterModule} from "@angular/router";
+import {CarsComponent} from './component/cars/cars.component';
+import {CarComponent} from './component/car/car.component';
+import {FullInfoComponent} from './component/full-info/full-info/full-info.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarsComponent,
+    CarComponent,
+    FullInfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([{
+      path: 'link/home', component: HomeComponent
+    },
+      {
+        path: 'link/cars', component: CarsComponent, children: [
+          {
+            path: ':id', component: FullInfoComponent
+          }
+        ]
+
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/models/Car.ts
@@ -0,0 +1,11 @@
+export interface Car {
+  id: number;
+  producer: string;
+  model: string;
+  year: number;
+  color: string;
+  type: string;
+  engine: string;
+  volume: number;
+  power: number;
+}
